7b0c9f3aa22f0009c62023e78ce394b3f489358d
pistachio_2011 <pistachio@yahoo.com>
2022-01-02 17:06:23 -0500

    add fixes for PNC Banks and USBanks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const parser = require('fast-csv')
 const filter = require('stream-filter')
+const moment = require('moment')
 
 // QIF Spec http://www.respmech.com/mym2qifw/qif_new.htm
 
@@ -16,8 +17,8 @@ fs.readdirSync(folderPath)
     let csvfile = path.join(folderPath, file);
     console.log("Processing " + csvfile);
     fs.createReadStream(csvfile)
-      .pipe(parser.parse({skipLines: 3,  ignoreEmpty: true }))
-      .on('data', (data) => { data.length > 9 ? results.push(data) : {} })
+      .pipe(parser.parse({ skipLines: `${config.csv.skipLines}`, ignoreEmpty: true }))
+      .on('data', (data) => { data.length > 1 ? results.push(data) : {} })
       .on('end', () => {
         const transactions = results
           .map(transaction => {
@@ -25,14 +26,17 @@ fs.readdirSync(folderPath)
             for (const key in config.csv.columns) {
               tx[`${key}`] = transaction[`${config.csv.columns[key]}`];
             }
-            tx.amount=`${tx.amount.replace(/\$/,'')}`
-            tx.category = config.bankCategories[`${tx.memo.toUpperCase().replace(/ .*/, '')}`]
-            tx.action = config.investment[`${tx.memo.toUpperCase().replace(/ .*/, '')}`]
+            tx.date = getSafeDate(tx.date)
+            tx.amount = `${tx.amount.replace(/\$/, '')}`
+            tx.category = calcCategory(tx.memo)
+            if (config.csv.type.includes("investment")) {
+              tx.action = calcAction(tx.memo);
+            }
             return tx
           });
 
         const bankTx = transactions
-          .filter(tx => tx.category)
+          .filter(tx => isBankTx(tx))
           .map(transaction => { return formatBankTx(transaction) })
           .join('\n\n');
         fs.writeFile(csvfile + '.bank.qif', '!Type:Bank\n' + bankTx,
@@ -41,47 +45,71 @@ fs.readdirSync(folderPath)
             console.log(csvfile + '.bank.qif created.')
           });
 
-        const investTx = transactions
-          .filter(tx => !tx.category)
-          .map(transaction => { return formatInvestTx(transaction) })
-          .join('\n\n');
-        fs.writeFile(csvfile + '.invest.qif', '!Type:Invst\n' + investTx,
-          (err) => {
-            if (err) console.log(err)
-            console.log(csvfile + '.bank.qif created.')
-          });
-      });
-  })
+        if (config.csv.type.includes("investment")) {
+          const investTx = transactions
+            .filter(tx => !isBankTx(tx))
+            .map(transaction => { return formatInvestTx(transaction) })
+            .join('\n\n');
+          fs.writeFile(csvfile + '.invest.qif', '!Type:Invst\n' + investTx,
+            (err) => {
+              if (err) console.log(err)
+              console.log(csvfile + '.bank.qif created.')
+            });
+        }
+      })
+    })
 
+    const getSafeDate = function (dateStr) {
+      if (dateStr && config.csv.dateFormat) {
+        const tradeDate = moment(dateStr, `${config.csv.dateFormat}`)
+        return tradeDate.format("MM/DD/YYYY")
+      }
+      return dateStr;
+    }
 
-const formatBankTx = function (tx) {
-  if (tx.category.includes('Interest') && !tx.payee) {
-    tx.payee = `${config.interestPayee}`
-  }
-  let qifText = ''
-  if (tx.date) qifText +=`\nD${tx.date}`;
-  if (tx.checknum) qifText += `\nN${tx.checknum}`
-  if (tx.amount) qifText += `\nT${tx.amount}`
-  if (tx.payee) qifText += `\nP${tx.payee}`
-  if (tx.category) qifText += `\nL${tx.category}`
-  if (tx.memo) qifText += `\nM${tx.memo}`
-  if (qifText.length > 0) qifText +=`\n^\n`
+    const calcCategory = function (str) {
+      return config.bankCategories[str.toUpperCase().replace(/ .*/, '')];
+    }
 
-  return qifText.trim()
-};
+    const calcAction = function (str) {
+      return config.investment[str.toUpperCase().replace(/ .*/, '')]
+    }
+    const isBankTx = function (tx) {
+      return config.csv.type.includes("bank") || tx.category
+    }
 
-const formatInvestTx = function (tx) {
-  let qifText = ''
-  if (tx.date) qifText +=`\nD${tx.date}`;
-  if (tx.action) qifText += `\nN${tx.action}`
-  if (tx.security) qifText += `\nY${tx.security}`
-  if (tx.quantity) qifText += `\nQ${tx.quantity}`
-  if (tx.amount) qifText += `\nT${tx.amount}`
-  if (tx.payee) qifText += `\nP${tx.payee}`
-  if (tx.memo) qifText += `\nM${tx.memo}`
-  if (tx.commission) qifText += `\nO${tx.commission}`
-  if (tx.account) qifText += `\nL${tx.account}`
-  if (qifText.length > 0) qifText +=`\n^\n`
-  return qifText.trim()
-}
+    const formatBankTx = function (tx) {
+      if (config.csv.type.includes("bank") && tx.category && tx.category.includes('Interest') && !tx.payee) {
+        tx.payee = `${config.interestPayee}`
+      }
+      let qifText = ''
+      if (tx.date) qifText += `\nD${tx.date}`;
+      if (tx.checknum) qifText += `\nN${tx.checknum}`
+      if (tx.withdrawls) {
+        qifText += `\nT${tx.withdrawls.replace(/\$/,'').replace(/,/,'') * -1}`
+      } else if (tx.amount) {
+        qifText += `\nT${tx.amount}`
+      }
+      if (tx.payee) qifText += `\nP${tx.payee}`
+      if (tx.category) qifText += `\nL${tx.category}`
+      if (tx.memo) qifText += `\nM${tx.memo}`
+      if (qifText.length > 0) qifText += `\n^\n`
+
+      return qifText.trim()
+    };
+
+    const formatInvestTx = function (tx) {
+      let qifText = ''
+      if (tx.date) qifText += `\nD${tx.date}`;
+      if (tx.action) qifText += `\nN${tx.action}`
+      if (tx.security) qifText += `\nY${tx.security}`
+      if (tx.quantity) qifText += `\nQ${tx.quantity}`
+      if (tx.amount) qifText += `\nT${tx.amount}`
+      if (tx.payee) qifText += `\nP${tx.payee}`
+      if (tx.memo) qifText += `\nM${tx.memo}`
+      if (tx.commission) qifText += `\nO${tx.commission}`
+      if (tx.account) qifText += `\nL${tx.account}`
+      if (qifText.length > 0) qifText += `\n^\n`
+      return qifText.trim()
+    }
 
