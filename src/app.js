73cdc18a90fa5059a0f2544124a31e204007fe0c
pistachio_2011 <pistachio@yahoo.com>
2022-01-08 19:43:29 -0500

    Enable unit test

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const QIFGenerator = require('./converter/qif-generator')
+const TransactionMapper = require('./services/transaction-mapper');
+const parser = require('fast-csv')
+
+// QIF Spec http://www.respmech.com/mym2qifw/qif_new.htm
+
+const args = process.argv.slice(2);
+const config = JSON.parse(fs.readFileSync(path.normalize(args[0])));
+
+const folderPath = path.normalize(args[1]);
+const stats = fs.statSync(folderPath);
+let csvfiles = [];
+if (stats.isDirectory()) {
+  fs.readdirSync(folderPath)
+    .filter(function (elm) { return elm.match(/.*\.(csv$)/ig); })
+    .forEach(file => {
+      let csvfile = path.join(folderPath, file);
+      csvfiles.push(csvfile);
+    });
+} else {
+  csvfiles.push(folderPath);
+}
+
+let txMapper = new TransactionMapper(config);
+if (!csvfiles || csvfiles.length == 0) {
+  console.log("No .csv file found under " + folderPath + ", check again?")
+}
+csvfiles.forEach(file => {
+  console.log('Processing ' + file);
+  const results = [];
+  const qif = new QIFGenerator();
+  fs.createReadStream(file)
+    .pipe(parser.parse({ skipLines: `${config.csv.skipLines}`, ignoreEmpty: true }))
+    .on('data', (data) => { data.length > 1 ? results.push(data) : {}; })
+    .on('end', () => {
+      const transactions = txMapper.map2tx(results);
+      qif.save2File(transactions, file);
+    });
+})
+
+
+
---
+++ src/converter/qif-generator.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+// QIF Spec http://www.respmech.com/mym2qifw/qif_new.htm
+class QIFGenerator {
+
+	save2File(transactions, filenamePrefix) {
+		if (transactions.bankTx) {
+
+			const bankTxString = transactions.bankTx.map(transaction => { return this.formatBankTx(transaction); })
+				.join('\n\n');
+			fs.writeFile(filenamePrefix + '.bank.qif', '!Type:Bank\n' + bankTxString,
+				(err) => {
+					if (err) console.log(err);
+					console.log(filenamePrefix + '.bank.qif created.');
+				});
+		}
+		if (transactions.investTx) {
+			const investTx = transactions.investTx
+				.map(transaction => { return this.formatInvestTx(transaction); })
+				.join('\n\n');
+			fs.writeFile(filenamePrefix + '.invest.qif', '!Type:Invst\n' + investTx,
+				(err) => {
+					if (err) console.log(err);
+					console.log(filenamePrefix + '.bank.qif created.');
+				});
+		}
+	};
+
+	formatBankTx(tx) {
+		let qifText = '';
+		if (tx.date) qifText += `\nD${tx.date}`;
+		if (tx.checknum) qifText += `\nN${tx.checknum}`;
+		if (tx.withdrawls) {
+			qifText += `\nT${tx.withdrawls.replace(/\$/, '').replace(/,/, '') * -1}`;
+		} else if (tx.amount) {
+			qifText += `\nT${tx.amount}`;
+		}
+		if (tx.payee) qifText += `\nP${tx.payee}`;
+		if (tx.category) qifText += `\nL${tx.category}`;
+		if (tx.memo) qifText += `\nM${tx.memo}`;
+		if (qifText.length > 0) qifText += '\n^\n';
+
+		return qifText.trim();
+	};
+
+	formatInvestTx(tx) {
+		let qifText = '';
+		if (tx.date) qifText += `\nD${tx.date}`;
+		if (tx.action) qifText += `\nN${tx.action}`;
+		if (tx.security) qifText += `\nY${tx.security}`;
+		if (tx.quantity) qifText += `\nQ${tx.quantity}`;
+		if (tx.amount) qifText += `\nT${tx.amount}`;
+		if (tx.payee) qifText += `\nP${tx.payee}`;
+		if (tx.memo) qifText += `\nM${tx.memo}`;
+		if (tx.commission) qifText += `\nO${tx.commission}`;
+		if (tx.account) qifText += `\nL${tx.account}`;
+		if (qifText.length > 0) qifText += '\n^\n';
+		return qifText.trim();
+	};
+
+}
+module.exports = QIFGenerator;
